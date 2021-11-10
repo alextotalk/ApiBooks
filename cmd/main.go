40cc8813f3233d233952471f8bca92cf2473dea4
Заяц Алексей <alexoo@inbox.ru>
2021-11-10 03:48:34 +0200

    :nerd_face:	Creat connection to database

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,33 @@ import (
 	"github.com/alextotalk/ApiBooks/pkg/handler"
 	"github.com/alextotalk/ApiBooks/pkg/repository"
 	"github.com/alextotalk/ApiBooks/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
 func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("error initialization config: %s", err.Error())
 	}
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
 
-	repos := repository.NewRepository()
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -1,5 +1,7 @@
 package repository
 
+import "github.com/jmoiron/sqlx"
+
 type Authorization interface {
 }
 type BooksStorage interface {
@@ -10,6 +12,6 @@ type Repository struct {
 	BooksStorage
 }
 
-func NewRepository() *Repository {
+func NewRepository(db *sqlx.DB) *Repository {
 	return &Repository{}
 }
